f6f128972feddffc887d4c1bf481483398dac17a
systemplado <systemplado@gmail.com>
2017-04-29 15:46:10 +0800

    updates: - added test cases for with initial board (2x2), clean boards (4x4, 8x8), invalid board (8x8) - added backtracking without duplicates - added backtracking for given initial board

--- project.c
+++ project.c
@@ -8,16 +8,14 @@ typedef struct node {
 } mov;
 
 int NO_OF_PUZZLES;
-FILE *FP;
+FILE *in, *out;
 
 int **board;
-int **board_init;
 int dimension;
 int chancellor_count;
 
 void printBoard(int **board, char *name, int log) {
     if (!log) return;
-
     int i, j;
 
     printf("[LOG] %s: \n", name);
@@ -28,6 +26,13 @@ void printBoard(int **board, char *name, int log) {
         }
         printf("\n");
     }
+}
+
+int isBefore(mov current, mov new) {
+    return (
+        current.row*dimension + current.col <
+        new.row*dimension + new.col
+    );
 }
 
 mov initializeMove(int row, int col) {
@@ -42,6 +47,8 @@ mov initializeMove(int row, int col) {
 int doMove(mov move, int what_move) {
     if (move.row < dimension && move.row > -1 && move.col < dimension && move.col > -1) {
         board[move.row][move.col] = what_move;
+
+        what_move ? chancellor_count++ : chancellor_count--;
         return 1;
     }
 
@@ -63,7 +70,6 @@ int checkMove(mov move, int log) {
 
     //check knight-move
     if (dimension < 3) {
-        chancellor_count++;
         return 1;
     }
 
@@ -93,7 +99,6 @@ int checkMove(mov move, int log) {
         }
     }
 
-    chancellor_count++;
     return 1;
 }
 
@@ -102,41 +107,68 @@ void initializeBoard() {
 
     chancellor_count = 0;
     board = (int **) malloc (sizeof(int *)*dimension);
-    board_init = (int **) malloc (sizeof(int *)*dimension);
 
     for (i=0; i<dimension; i++) {
         board[i] = (int *) malloc (sizeof(int)*dimension);
-        board_init[i] = (int *) malloc (sizeof(int)*dimension);
         for (j=0; j<dimension; j++) {
-            fscanf(FP, "%d", &board[i][j]);
-            board_init[i][j] = board[i][j];
+            fscanf(in, "%d", &board[i][j]);
             if (board[i][j]) chancellor_count++;
         }
     }
 
-    printBoard(board, "board AND board_init", 1);
+    printBoard(board, "board", 1);
 }
 
 int backtrack() {
-    int log_move=0, log=1;
-    int i, j, start, nth_move, solutions_count=0;
+    int log_move, log;
+    int i, j, start, nth_move, solutions_count;
     int tos[dimension+2];
     mov candidate, **solutions;
-    FILE *out;
 
     printf("[LOG] backtracking...\n");
 
-    solutions = (mov **) malloc (sizeof(mov *)*(dimension+2));
+    if (chancellor_count > dimension) {
+        return 0;
+    }
 
+    if (chancellor_count == dimension) {
+        int board_temp[dimension][dimension];
+
+        for (i=0; i<dimension; i++) {
+            for (j=0; j<dimension; j++) {
+                board_temp[i][j] = board[i][j];
+                board[i][j] = 0;
+            }
+        }
+
+        for (i=0; i<dimension; i++) {
+            for (j=0; j<dimension; j++) {
+                if (board_temp[i][j]) {
+                    mov move = initializeMove(i, j);
+                    if(checkMove(move, 1)) {
+                        board[i][j] = 1;
+                    }
+                    else {
+                        return 0;
+                    }
+                }
+
+            }
+        }
+    }
+
+    solutions = (mov **) malloc (sizeof(mov *)*(dimension+2));
     for(i=0; i<dimension+2; i++) {
         solutions[i] = (mov *) malloc (sizeof(mov )*(dimension*dimension+2));
     }
 
-    out = fopen("solutions.out", "w");
-
     start = chancellor_count;
     nth_move = start;
+    log_move = 0;
+    log = 0;
     tos[start] = 1;
+    solutions_count = 0;
+
     while(tos[start]>0) {
         if (tos[nth_move]>0) {
             nth_move++;
@@ -145,23 +177,21 @@ int backtrack() {
             if (nth_move==dimension+1) {
                 candidate = solutions[nth_move-1][tos[nth_move-1]];
                 doMove(candidate, 1);
-                //printBoard(board, "solution", 1);
+
+                fprintf(out, "%dth solution\n", ++solutions_count);
 
                 for (i=0; i<dimension; i++) {
                     for (j=0; j<dimension; j++) {
                         fprintf(out, "%d ", board[i][j]);
+                        if (log) printf("%d ", board[i][j]);
                     }
+                    if (log) printf("\n");
                     fprintf(out, "\n");
                 }
 
-                fprintf(out, "-\n");
-
-                for(i=start+1; i<=dimension; i++) {
-                    printf("[LOG] steps: %d, %d\n", solutions[i][tos[i]].row, solutions[i][tos[i]].col);
-                }
-
-                solutions_count++;
-
+                if (log)
+                    for(i=start+1; i<=dimension; i++)
+                        printf("[LOG] steps: %d, %d\n", solutions[i][tos[i]].row, solutions[i][tos[i]].col);
             }
 
             else if (nth_move==1) { //initialize first move
@@ -188,7 +218,7 @@ int backtrack() {
                     for (j=dimension-1; j>-1; j--) {
                         if (!board[i][j]) {
                             mov move = initializeMove(i,j);
-                            if (checkMove(move, 0)) {
+                            if (checkMove(move, 0) && isBefore(candidate, move)) {
                                 tos[nth_move]++;
                                 solutions[nth_move][tos[nth_move]] = move;
                                 if (log_move) printf("[LOG] %d-th stack; tos @ %d; add(%d, %d)\n", nth_move, tos[nth_move], move.row, move.col);
@@ -208,38 +238,39 @@ int backtrack() {
             if (log_move) printf("[LOG] %d-th stack; tos @ %d; undo(%d, %d)\n", nth_move, tos[nth_move], candidate.row, candidate.col);
 
             tos[nth_move]--;
-
         }
     }
 
     free(solutions);
-    fclose(out);
 
     printf("[LOG] backtracked with %d solutions...\n", solutions_count);
     return solutions_count;
 }
 
 void main() {
-    int i;
+    int i, j;
 
-    FP = fopen("project.in", "r");
+    in = fopen("project.in", "r");
+    out = fopen("solutions.out", "w");
 
-    fscanf(FP, "%d \n", &NO_OF_PUZZLES);
+    fscanf(in, "%d \n", &NO_OF_PUZZLES);
     printf("[LOG] NO_OF_PUZZLES: %d\n", NO_OF_PUZZLES);
 
-
     for (i=0; i<NO_OF_PUZZLES; i++) {
-        fscanf(FP, "%d \n", &dimension);
+        printf("\n[LOG] PUZZLE-%d:\n", i);
+        fscanf(in, "%d \n", &dimension);
         printf("[LOG] dimension: %d\n", dimension);
         initializeBoard();
         if (!backtrack()) {
             printf("[LOG] there are no possible solutions\n");
         }
 
-        for(i=0; i<dimension; i++)
-            free(board[i]);
+        for(j=0; j<dimension; j++) {
+            free(board[j]);
+        }
         free(board);
     }
 
-    fclose(FP);
+    fclose(in);
+    fclose(out);
 }
